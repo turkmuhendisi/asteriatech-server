58575c1f22105406094011a55380aeccfb1601d9
turkmuhendisi <1turkmuhendisi@gmail.com>
2024-07-06 16:28:45 +0300

    Project created and uploaded to Github

---
+++ src/main/java/com/teknofest/asteriatech_server/AsteriatechServerApplication.java
@@ -0,0 +1,13 @@
+package com.teknofest.asteriatech_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteriatechServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteriatechServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teknofest/asteriatech_server/AsteriatechServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teknofest.asteriatech_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteriatechServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
